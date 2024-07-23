a9dcb5060e412f573cdfc06ad4d1dc0c42e5b3a0
Zanya2513 <2206375@kiit.ac.in>
2024-07-24 00:21:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+    document.getElementById('response').innerHTML = "Yay! I can't wait!";
+});
+
+document.getElementById('noBtn').addEventListener('mouseover', moveButton);
+document.getElementById('noBtn').addEventListener('click', showNoMessage);
+
+function moveButton() {
+    const button = document.getElementById('noBtn');
+    const container = document.querySelector('.container');
+    const containerRect = container.getBoundingClientRect();
+
+    const newTop = Math.random() * (containerRect.height - button.offsetHeight);
+    const newLeft = Math.random() * (containerRect.width - button.offsetWidth);
+
+    button.style.top = `${newTop}px`;
+    button.style.left = `${newLeft}px`;
+}
+
+function showNoMessage() {
+    document.getElementById('response').innerHTML = "No tumhare liye option h hi nhi";
+}
